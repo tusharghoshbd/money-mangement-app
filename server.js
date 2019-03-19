f3a741cbb8d371ab68ed2dc10a03e4f0c6ca8785
DESKTOP-DD9J1NR\Tushar Ghosh <tusharghosh09006@gmail.com>
2019-03-19 21:49:56 +0600

    money mangement app initial commit
    setup the react client and nodejs and mongodb

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res)=>{
+
+    res.json({
+        message: "Welcome To Our Application"
+    })
+
+})
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+    mongoose.connect('mongodb://localhost:27017/money-mangement-app', {useNewUrlParser: true},()=>{
+        console.log("Database is Connected.");
+    });
+});
+
+
